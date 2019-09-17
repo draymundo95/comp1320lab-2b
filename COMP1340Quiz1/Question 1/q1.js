da3dd0a59d60bd5fede67f165f3008bf65661638
Denise Raymundo <deniseraymundo@denises-mbp.wifi.bcit.ca>
2019-09-17 14:17:17 -0700

    first commit

---
+++ COMP1340Quiz1/Question 1/q1.js
@@ -0,0 +1,4 @@
+*/ Question 1?
+*/ git is a tool used on a CPU to track a user's code/code files whereas
+*/ github is an online tool/website used to track, step-by-step, a user's
+*/ code, it's history and how the code was made up until that point. 
